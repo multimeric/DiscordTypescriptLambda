4916be07620c4dff1bef46dc517b82f682d83f11
Michael Milton <michael.r.milton@gmail.com>
2022-02-04 12:23:11 +1100

    Remove .d.ts files

--- lib/command.d.ts
+++
@@ -1,4 +0,0 @@
-import { SlashCommand, SlashCommandOptions, SlashCreator } from "slash-create";
-export default class LambdaSlashCommand extends SlashCommand {
-  constructor(creator: SlashCreator, opts: SlashCommandOptions);
-}
--- lib/commands/hello.d.ts
+++
@@ -1,6 +0,0 @@
-import { CommandContext, SlashCreator } from "slash-create";
-import LambdaSlashCommand from "../command";
-export default class HelloCommand extends LambdaSlashCommand {
-  constructor(creator: SlashCreator);
-  run(ctx: CommandContext): Promise<string>;
-}
