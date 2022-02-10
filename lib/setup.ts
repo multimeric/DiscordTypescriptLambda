83334756350ace311517adb9065192d2c734cfbb
Michael Milton <michael.r.milton@gmail.com>
2022-02-10 22:43:01 +1100

    Await the command sync

--- lib/setup.ts
+++ lib/setup.ts
@@ -4,7 +4,7 @@ import { commands, getSlashEnv } from "./common";
 export async function handler() {
   // Sync all commands before publishing
   const creator = new SlashCreator(getSlashEnv());
-  creator.registerCommands(commands).syncCommands();
+  await creator.registerCommands(commands).syncCommandsAsync();
 
   /*
     Put your setup actions here, e.g. loading data into a database
